0450674cc8ea3b1d45952a27a9726ee34759010a
kelseyp99 <kelseyp99@example.com>
2025-03-24 21:15:03 -0400

    Forced overwrite of FakeAdMobBanner to plain View, no images

--- src/components/FakeAdMobBanner.tsx
+++ src/components/FakeAdMobBanner.tsx
@@ -1,28 +1,6 @@
-import React from "react";
-import { View, Image, StyleSheet } from "react-native";
-
 const FakeBanner = () => {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/images/WholeFoods.png")} style={styles.banner} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#004225", // Matching Whole Foods theme
-    paddingVertical: 2, // Further reduce vertical padding
-    height: 60, // Explicitly set a height to control the banner area
-  },
-  banner: {
-    width: "100%",
-    height: 40, // Reduce height to make the banner smaller
-    resizeMode: "contain",
-  },
-});
-
-export default FakeBanner;
