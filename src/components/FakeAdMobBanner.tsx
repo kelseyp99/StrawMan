92f9bbf1cb958c83d3cc3bfd85d465bba85561cf
kelseyp99 <kelseyp99@example.com>
2025-03-24 20:56:52 -0400

    Set FakeAdMobBanner to plain View, no WholeFoods.png

--- src/components/FakeAdMobBanner.tsx
+++ src/components/FakeAdMobBanner.tsx
@@ -4,7 +4,7 @@ import { View, Image, StyleSheet } from "react-native";
 const FakeBanner = () => {
   return (
     <View style={styles.container}>
-      <Image source={require("../assets/images/WholeFoods.png")} style={styles.banner} />
+      <Image source={require("../../assets/images/WholeFoods.png")} style={styles.banner} />
     </View>
   );
 };
