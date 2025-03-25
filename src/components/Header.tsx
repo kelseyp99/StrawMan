755ce504b77555f95cbf7f2a9ba5f6c45dbe29c9
kelseyp99 <kelseyp99@example.com>
2025-03-24 21:37:50 -0400

    Moved all images to assets/images/ and fixed paths to /assets/images/

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { View, Text, Image } from "react-native";
 export default function Header() {
   return (
     <View style={{ alignItems: "center", marginBottom: 20 }}>
-      <Image source={require("../../src/assets/images/ask-janet-icon.png")} style={{ width: 100, height: 100, marginBottom: 10 }} />
+      <Image source={require("/assets/images/ask-janet-icon.png")} style={{ width: 100, height: 100, marginBottom: 10 }} />
       <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center" }}>Ask Janet</Text>
     </View>
   );
