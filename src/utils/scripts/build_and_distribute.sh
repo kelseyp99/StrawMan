c8065ad73161a220894c0e5b0d5754ed5e0da843
Phil Kelsey <werkhardor@gmail.com>
2025-04-11 09:45:18 -0400

    Automated commit: Update and build

--- src/utils/scripts/build_and_distribute.sh
+++ src/utils/scripts/build_and_distribute.sh
@@ -23,7 +23,7 @@ export PATH=$PATH:$ANDROID_HOME/platform-tools
 rm -rf ~/.gradle/caches ~/.eas/build
 
 echo "Running expo-doctor to check and fix dependencies..."
-npx expo-doctor --fix all
+npx expo-doctor --fix
 if [ $? -eq 0 ]; then
     echo "Expo doctor completed successfully!"
 else
