46c6c8188262fed9cc98895d4a665a1300548b7c
Phil Kelsey <werkhardor@gmail.com>
2025-04-23 19:58:25 -0400

    Fix WholeFoods.png path in FakeAdMobBanner.tsx

--- src/components/FakeAdMobBanner.tsx
+++ src/components/FakeAdMobBanner.tsx
@@ -1,27 +1,30 @@
-import React from "react";
-import { View, Image } from "react-native";
-import { StyleSheet } from "react-native";
+import React from 'react';
+import { View, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const FakeBanner = () => {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/images/WholeFoods.png")} style={styles.banner} />
+      <Image
+        source={require('../../../assets/images/WholeFoods.png')}
+        style={styles.banner}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
+    width: '100%',
     height: 100,
-    backgroundColor: "#f0f0f0",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   banner: {
     width: 200,
     height: 50,
-    resizeMode: "contain",
+    resizeMode: 'contain',
   },
 });
 
