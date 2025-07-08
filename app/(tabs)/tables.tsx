1c2b0dfbb0504d0758f821e8487700056589d594
kelseyp99 <kelseyp99@example.com>
2025-07-08 14:52:32 -0400

    WIP: Prepare for main merge and EAS cloud build

--- app/(tabs)/tables.tsx
+++ app/(tabs)/tables.tsx
@@ -1,14 +1,11 @@
 import MainComponent from '../../src/components/Tables';
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const TablesScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <MainComponent />
-      </GestureHandlerRootView>
+      <MainComponent />
     </SafeAreaView>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,23 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AuthProvider } from './context/AuthContext';
 import { SyncProvider } from './context/SyncContext';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <SyncProvider>
-        <Stack initialRouteName="splash">
-          <Stack.Screen name="splash" options={{ headerShown: false }} />
-          <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-          <Stack.Screen name="login" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="settings" options={{ title: 'Settings' }} />
-        </Stack>
-      </SyncProvider>
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <SyncProvider>
+          <Stack initialRouteName="splash">
+            <Stack.Screen name="splash" options={{ headerShown: false }} />
+            <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+            <Stack.Screen name="login" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="settings" options={{ title: 'Settings' }} />
+          </Stack>
+        </SyncProvider>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
